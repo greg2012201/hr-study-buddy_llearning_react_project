b577a61cd1219db8a2fd0351853311260403bd51
Grzegorz Dubiel <greg2012201@gmail.com>
2021-03-17 14:34:18 +0100

    UsersLIst-finall

---
+++ src/components/UsersList/UsersList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { users } from 'data/users'
+
+import UsersListItem from 'components/UsersListItem/UsersListItem'
+
+const UsersList = () => (
+  <div>
+    <ul>
+      {users.map((userData) => (
+        <UsersListItem userData={userData} /> // tutaj przekazujemy propsa przy użyciu map
+      ))}
+    </ul>
+  </div>
+)
+
+export default UsersList
---
+++ src/data/users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'Grzegorz Dubiel',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Alicja Nowak',
+    attendance: '10%',
+    average: '3.3',
+  },
+  {
+    name: 'Paweł Andrzejewski',
+    attendance: '60%',
+    average: '8',
+  },
+  {
+    name: 'Anna Nowak',
+    attendance: '90%',
+    average: '5',
+  },
+  {
+    name: 'Katarzyna Świątek',
+    attendance: '60%',
+    average: '4',
+  },
+]
