b25100a116d9280783081e72a5c703b1a89167f6
Grzegorz Dubiel <greg2012201@gmail.com>
2021-03-22 13:38:07 +0100

    Add the event to the button component

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -4,16 +4,20 @@ import Button from 'components/atoms/Button/Button'
 import Average from 'components/atoms/Average/Average'
 import { Wrapper, Info } from './UsersListItem.styles'
 
-const UsersListItem = ({ userData: { average, name, attendance } }) => (
-  <Wrapper>
-    <Average averages={average} />
-    <Info>
-      <p>{name}</p>
-      <p>attendance: {attendance}</p>
-    </Info>
-    <Button />
-  </Wrapper>
-)
+const UsersListItem = ({ index, userData: { average, name, attendance } }) => {
+  const showIndex = (index) => alert(`This is student #${index + 1}`)
+
+  return (
+    <Wrapper>
+      <Average averages={average} />
+      <Info>
+        <p>{name}</p>
+        <p>attendance: {attendance}</p>
+      </Info>
+      <Button onClick={() => showIndex(index)} />
+    </Wrapper>
+  )
+}
 
 UsersListItem.propTypes = {
   userData: PropTypes.shape({
