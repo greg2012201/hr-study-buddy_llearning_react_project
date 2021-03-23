92e2885e1c73f889bc132a2f2c2e37c2c4277a74
Grzegorz Dubiel <greg2012201@gmail.com>
2021-03-23 14:36:02 +0100

    components-lifecycle-final

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,15 +1,61 @@
-import React from 'react'
-import { users } from 'data/users'
+import React, { useState, useEffect } from 'react'
+import { users as usersData } from 'data/users'
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem'
 import { Wrapper } from './UsersList.styles'
-const UsersList = () => (
-  <Wrapper>
-    <ul>
-      {users.map((userData, i) => (
-        <UsersListItem index={i} userData={userData} /> // tutaj przekazujemy propsa przy użyciu map
-      ))}
-    </ul>
-  </Wrapper>
-)
+
+const mockAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (usersData) {
+        resolve([...usersData])
+      } else {
+        reject({ message: 'Error' })
+      }
+    }, 2000)
+  })
+}
+
+const UsersList = () => {
+  const [users, setUsers] = useState([])
+  const [isLoading, setLoadingState] = useState([])
+
+  useEffect(() => {
+    setLoadingState(true)
+
+    mockAPI()
+      .then((data) => {
+        setLoadingState(false)
+        setUsers(data)
+      })
+      .catch((err) => console.log(err))
+    /*  return () => {
+      window.removeEventListener('event', function) // jako przykład
+    } */
+  }, [])
+
+  useEffect(() => {
+    console.log('Loading state has changed')
+  }, [isLoading])
+
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name)
+    setUsers(filteredUsers)
+  }
+  return (
+    <Wrapper>
+      <h1>{isLoading ? 'Loading...' : 'Users List'}</h1>
+      <ul>
+        {users.map((userData, i) => (
+          <UsersListItem
+            deleteUser={deleteUser}
+            key={userData}
+            index={i}
+            userData={userData}
+          />
+        ))}
+      </ul>
+    </Wrapper>
+  )
+}
 
 export default UsersList
