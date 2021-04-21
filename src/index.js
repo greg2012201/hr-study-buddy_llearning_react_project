5ab4e6901fb2d9dadcbbf3dd30d30e10c7c84925
Grzegorz Dubiel <greg2012201@gmail.com>
2021-04-21 21:26:02 +0200

    Dependencies update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from 'views/Root';
-import 'assets/styles/fonts.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from 'views/Root'
+import 'assets/styles/fonts.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
