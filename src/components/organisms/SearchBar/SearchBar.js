aa2764a97135c6d9a82ed9bff9fb3c2e66f65537
Grzegorz Dubiel <greg2012201@gmail.com>
2021-04-26 13:01:17 +0200

    Create a basic search engine for students on the frontend side

--- src/components/organisms/SearchBar/SearchBar.js
+++ src/components/organisms/SearchBar/SearchBar.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from 'components/atoms/Input/Input'
 import { SearchBarWrapper, StatusInfo } from './SearchBar.style'
+import axios from 'axios'
+import { useParams } from 'react-router'
 const SearchBar = () => {
+  const { id } = useParams()
+  const [searchedStudents, setSearchedStudents] = useState([])
+  const handleOnChange = (e) => {
+    axios
+      .post(`/students/search`, { searchPhrase: e.target.value })
+      .then(({ data: { students } }) => setSearchedStudents(students))
+      .catch((err) => console.log(err))
+  }
+  useEffect(() => {}, [])
   return (
     <SearchBarWrapper>
       <StatusInfo>
@@ -9,8 +20,11 @@ const SearchBar = () => {
         <p>
           <strong>Teacher</strong>
         </p>
+        {searchedStudents.map(({ name }) => (
+          <p>{name}</p>
+        ))}
       </StatusInfo>
-      <Input />
+      <Input onChange={handleOnChange} />
     </SearchBarWrapper>
   )
 }
